634db9ec42c61c5f5876057245653e290628f3a3
Fredrick Atkinson <taylynatkinson@gmail.com>
2023-04-24 16:07:29 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+
+storeItems.forEach(function(item) {
+    if (item.inStock) {
+        let itemHTML = `
+            <div class= "item"  id= "container">
+            <p class ="price">$${item.price}</p>        
+            <p class ="name">${item.name}</p>   
+            <p class ="details">${item.details}</p> 
+            </div>`;
+        $("#store").append(itemHTML);
+    }
+});
+
+
+$("#toggle-dark-mode").click(function() {
+    $("body").toggleClass("dark-mode");
+   $('.stock').toggleClass("dark-mode");
+   $('#toggle-dark-mode').toggleClass("dark-mode");
+   $("h1").toggleClass("dark-mode");
+});
